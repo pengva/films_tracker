7b2559ae541ec7449b10ceaca12e9c398489c37f
armatura24 <vivcharenko10@gmail.com>
2023-04-27 03:49:57 +0300

    setup env

--- config/application.rb
+++ config/application.rb
@@ -11,12 +11,16 @@ module FilmsTracker
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
-    # config.before_configuration do
-    #   env_file = File.join(Rails.root, 'config', 'local_env.yml')
-    #   YAML.load(File.open(env_file)).each do |key, value|
-    #     ENV[key.to_s] = value
-    #   end
-    # end
+    config.before_configuration do
+      env_file = File.join(Rails.root, 'config', 'local_env.yml')
+      if File.exist?(env_file)
+        YAML.load(File.open(env_file)).each do |key, value|
+          ENV[key.to_s] = value
+        end
+      end
+
+      Rails.application.routes.default_url_options[:host] = ENV['HOST']
+    end
 
     # Configuration for the application, engines, and railties goes here.
     #
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,6 +16,24 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
+  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_options = { from: ENV['GMAIL_USERNAME'] }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { :host => ENV['HOST'], protocol: 'https' }
+
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "ukd.edu.ua",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV["GMAIL_USERNAME"],
+    password: ENV["GMAIL_PASSWORD"]
+
+  }
+
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
   # config.require_master_key = true
